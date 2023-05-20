d9542408a0b67777e0dbb7253a982e14e355ea5f
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2023-05-20 20:14:07 +0400

    catch reading file error

--- src/app/api/days/route.ts
+++ src/app/api/days/route.ts
@@ -4,8 +4,13 @@ import path from 'path';
 
 export async function GET() {
   const filePath = path.join(process.cwd(), 'src/utils/mockedEvents.json');
-  const fileContents = fs.readFileSync(filePath, 'utf-8');
-  const data = JSON.parse(fileContents);
 
-  return NextResponse.json(data.events);
+  try {
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+    const data = JSON.parse(fileContents);
+    return NextResponse.json(data.events);
+  } catch (error) {
+    console.error('Failed to read file:', error);
+    return NextResponse.error();
+  }
 }
