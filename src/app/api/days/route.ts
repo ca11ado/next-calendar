1744c7344c138d8dafa7f026692ef44b96ac0975
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2023-05-21 13:07:21 +0400

    add default

--- src/app/api/days/route.ts
+++ src/app/api/days/route.ts
@@ -4,7 +4,7 @@ import { get } from "@vercel/edge-config";
 export async function GET() {
   try {
     const events = await get("events");
-    return NextResponse.json(events);
+    return NextResponse.json(events || []);
   } catch (error) {
     console.error("Failed to get value from Edge Config:", error);
     return NextResponse.error();
