6aa1948e85e8ef9a7573d6e742c15e8fab903b40
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2023-05-21 13:11:51 +0400

    one more default

--- src/app/api/days/route.ts
+++ src/app/api/days/route.ts
@@ -7,6 +7,6 @@ export async function GET() {
     return NextResponse.json(events || []);
   } catch (error) {
     console.error("Failed to get value from Edge Config:", error);
-    return NextResponse.error();
+    return NextResponse.json([]);
   }
 }
