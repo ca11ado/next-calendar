8993cd9c0ccf070428e3c16f2d01ae90b568c2b7
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2023-05-22 00:33:51 +0400

    check issue https://stackoverflow.com/questions/76285120/error-dynamic-server-usage-headers-on-next-13-4
    fixes DYNAMIC_SERVER_USAGE

--- src/app/api/days/route.ts
+++ src/app/api/days/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import { get } from "@vercel/edge-config";
 import { EVENTS_KEY } from "@/config";
 
+// FIXME: https://stackoverflow.com/questions/76285120/error-dynamic-server-usage-headers-on-next-13-4
+// безо всякой причины стала генериться ошибка для этой страницы, удалось поправить только добавлением этого экспорта
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     const events = await get(EVENTS_KEY);
