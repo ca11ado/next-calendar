1c3095ddd5e13a2c885852915a9054e2b5eef552
Khimtsov Anton <projects.tlt@yandex.ru>
2024-05-04 23:21:56 +0400

    trying to add CORS

--- app/api/v2/events/route.ts
+++ app/api/v2/events/route.ts
@@ -4,6 +4,8 @@ import { handleRequestError } from "@/utils/server/handleRequestError";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // defaults to auto
+
 export async function GET() {
   try {
     const loggedCustomer = await checkAuthUser();
@@ -11,7 +13,16 @@ export async function GET() {
       throw new WrongClientData("Not auth", 401);
     }
     const { rows: data } = await sql`SELECT * from events`;
-    return NextResponse.json({ status: "ok", data });
+    return new Response(JSON.stringify({ status: "ok", data }), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Content-Type": "application/json",
+      },
+    });
   } catch (e) {
     const { error, status } = handleRequestError(e);
     return NextResponse.json({ error }, { status });
