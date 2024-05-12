9b0541bdef4a7d43086643b9565b89ff78987873
Khimtsov Anton <projects.tlt@yandex.ru>
2024-05-12 23:08:44 +0400

    add colors

--- app/domains/events/features/calendar/components/Events.tsx
+++ app/domains/events/features/calendar/components/Events.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Event } from "@/domains/events/types/Event";
+import { getColorByType } from "@/domains/events/utils/getColorByType";
 
 const getDate = (date: string) => new Date(date).toLocaleDateString();
 
@@ -9,11 +10,15 @@ type Props = {
 
 const Events: React.FC<Props> = (props) => {
   const { events } = props;
+  const colorsByType = getColorByType(events);
   return (
     <div className="bg-white shadow-md rounded-lg p-4 max-w-sm">
       <ul className="list-none space-y-2">
         {events.map((event) => (
-          <li className="border-b border-gray-200 pb-2 mb-2">
+          <li
+            className="border-b border-gray-200 pb-2 mb-2"
+            style={{ backgroundColor: colorsByType[event.type] }}
+          >
             {getDate(event.start_at)} - {getDate(event.end_at)} -- {event.name}{" "}
             {event.type}
           </li>
@@ -22,11 +27,5 @@ const Events: React.FC<Props> = (props) => {
     </div>
   );
 };
-
-function getRandomColor() {
-  const colors = ["#FFC0CB", "#FFD700", "#90EE90", "#87CEEB", "#BA55D3"];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-}
 
 export default Events;
---
+++ app/domains/events/utils/getColorByType.ts
@@ -0,0 +1,36 @@
+import { Event } from "../types/Event";
+
+function getRandomColor() {
+  const colors = [
+    "#FFC0CB",
+    "#FFD700",
+    "#90EE90",
+    "#87CEEB",
+    "#BA55D3",
+    "#07988c",
+    "#9c0025",
+    "#77011d",
+    "#d209ed",
+    "#52055c",
+    "#e308ff",
+  ];
+  return () => {
+    if (!colors.length) {
+      throw new Error("Not enough colors, should create more");
+    }
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors.splice(randomIndex, 1)[0];
+  };
+}
+
+export const getColorByType = (
+  events: Event[]
+): Record<Event["type"], string> => {
+  const getColor = getRandomColor();
+  return events
+    .map(({ type }) => type)
+    .reduce<Record<Event["type"], string>>((accum, current) => {
+      accum[current] = accum[current] || getColor();
+      return accum;
+    }, {});
+};
