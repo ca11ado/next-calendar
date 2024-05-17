e40b2a39f62cb56da14a715cb2d565011bcb683b
Khimtsov Anton <projects.tlt@yandex.ru>
2024-05-17 23:21:52 +0400

    make function work with tests

--- app/domains/events/utils/getDateBySqueryId.spec.ts
+++ app/domains/events/utils/getDateBySqueryId.spec.ts
@@ -2,20 +2,20 @@ import { expect, test } from "vitest";
 import { getDateBySqueryId } from "./getDateBySqueryId";
 
 test.each([
-  [1, 1, 7, { startDate: 1, endDate: 1 }],
-  [1, 1, 7, { startDate: 1, endDate: 1 }],
-])("#% check generated function", (a, b, c, expected) => {
-  expect(getDateBySqueryId(a, new Date(b), c)).toBe(expected);
-});
-
-test.each([
-  [1, 1, 7, { startDate: 1, endDate: 1 }],
-  [1, 1, 7, { startDate: 1, endDate: 1 }],
+  [1, "2023-05-17", 7, { startDate: "2023-05-17", endDate: "2023-05-24" }],
+  [2, "2023-05-17", 7, { startDate: "2023-05-25", endDate: "2023-06-01" }],
+  [3, "2023-05-17", 7, { startDate: "2023-06-02", endDate: "2023-06-09" }],
+  [4, "2023-05-17", 7, { startDate: "2023-06-10", endDate: "2023-06-17" }],
+  [5, "2023-05-17", 7, { startDate: "2023-06-18", endDate: "2023-06-25" }],
+  [6, "2023-05-17", 7, { startDate: "2023-06-26", endDate: "2023-07-03" }],
+  [7, "2023-05-17", 7, { startDate: "2023-07-04", endDate: "2023-07-11" }],
+  [8, "2023-05-17", 7, { startDate: "2023-07-12", endDate: "2023-07-19" }],
+  [9, "2023-05-17", 7, { startDate: "2023-07-20", endDate: "2023-07-27" }],
+  [10, "2023-05-17", 7, { startDate: "2023-07-28", endDate: "2023-08-04" }],
+  [11, "2023-05-17", 7, { startDate: "2023-08-05", endDate: "2023-08-12" }],
 ])("add(%i, %i) -> %i", (a, b, c, expected) => {
-  expect(a + b).toBe(expected);
+  expect(getDateBySqueryId(a, new Date(b), c)).toEqual({
+    startDate: new Date(expected.startDate),
+    endDate: new Date(expected.endDate),
+  });
 });
-
-// this will return
-// ✓ add(1, 1) -> 2
-// ✓ add(1, 2) -> 3
-// ✓ add(2, 1) -> 3
--- app/domains/events/utils/getDateBySqueryId.ts
+++ app/domains/events/utils/getDateBySqueryId.ts
@@ -1,13 +1,17 @@
 import { addDays } from "date-fns";
 
-// TODO: add tests with vite
 export const getDateBySqueryId = (
   id: number,
   startDate: Date,
   daysRatio: number = 7
 ) => {
+  if (id <= 0) {
+    throw new Error("Id is not valid");
+  }
+  const add = (id - 1) * (daysRatio + 1);
+  const start = id === 1 ? startDate : addDays(startDate, add);
   return {
-    startDate: id === 1 ? startDate : addDays(startDate, id),
-    endDate: addDays(startDate, id * daysRatio),
+    startDate: start,
+    endDate: addDays(start, daysRatio),
   };
 };
